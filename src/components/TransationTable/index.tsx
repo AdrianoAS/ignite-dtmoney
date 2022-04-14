2059aab25ad682e5c15979994c9c9cf538b15a3b
AdrianoAS <aadriano990@gmail.com>
2022-04-13 23:29:38 -0300

    Adicionado dependencia do miragejs para fazer requisições fakes

--- src/components/TransationTable/index.tsx
+++ src/components/TransationTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./style";
 
 export function TransationTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transations")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
